44e199f1049fd67f76bf5cf82c3296eca4ab4f65
HammadNasir <hns.billionaires@gmail.com>
2025-12-25 08:10:38 +0530

    fixing 404 error - 25122025-0810

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -4,13 +4,13 @@ import PrivacyPolicy from "./pages/PrivacyPolicy";
 import Terms from "./pages/Terms";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
-import Calculator from "./pages/Calculator";
+import CalculatorPage from "./pages/Calculator";
 
 export default function AppRouter() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/calculator" element={<Calculator />} />
+      <Route path="/calculator" element={<CalculatorPage />} />
       <Route path="/privacy" element={<PrivacyPolicy />} />
       <Route path="/terms" element={<Terms />} />
       <Route path="/about" element={<About />} />
